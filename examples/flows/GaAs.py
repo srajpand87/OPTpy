e76e80cd52cd8df98db5879e4805c6c505526e74
trangel <trangel@lbl.gov>
2017-11-01 11:47:52 -0700

    example changed

--- examples/flows/GaAs.py
+++ examples/flows/GaAs.py
@@ -3,45 +3,55 @@ Compute optical responses with OPTpy
 
 """
 from OPTpy import  OPTflow,Structure
-from myLRC import mkjob_by_task
+#from myLRC import mkjob_by_task
 
 flow = OPTflow(
     dirname = './',
 
-#   Common variables:
+    #
+    # Common variables:
+    #
     prefix = 'gaas',      # Root name for files required by Tiniba
-#   Structure from file:
-    structure=Structure.from_file('../data/structures/GaAs.json'),
+    # Structure from file:
+    structure=Structure.from_file('.GaAs.cif'),
 
-#   Pseudopotentials:
-    pseudo_dir = '../data/pseudos',
+    #
+    # Pseudopotentials:
+    #
+    pseudo_dir = './',
     pseudos = ['31-Ga.pspnc', '33-As.pspnc'],
 
-    ecut = 15.0,           # Wavefunctions cutoff energy
+    ecut = 15.0,          # Cutoff energy for wavefunctions
     nspinor = 2,          # Number of spinorial components
 
-#   Variables for density:
+    #
+    # Variables for density:
+    #
     ngkpt = [4,4,4],      # k-point grid for density
     kshift = [.5,.5,.5],  # k-point shift for density
 
-#   Variables for momentum matrix elements and responses:
+    #
+    # Variables for momentum matrix elements and responses:
+    #
     kgrid_response=[4,4,4], # k-point grid for responses
-    nband=36,   # Number of bands
-    nbdbuf=2,   # Bands in buffer (see Abinit documentation).
-    nval_total=8,     #TODO get number of valence bands from pseudopotentials
+    nband=36,      # Total number of bands
+    nbdbuf=2,      # Bands in buffer (see Abinit documentation)
+    nval_total=8,  # Total number of valence bands
 
-#   Variables for responses:
-#   Bands to include for transitions (can be less than for RPMNS)
-    ncond=8,
-    nval=8,
-#   Response to calculate, see Doc. in responses.py
+    #
+    # Variables for responses:
+    #
+    # Bands to include for transitions (can be less than for RPMNS)
+    ncond=8,         # Number of conduction bands to include
+    nval=8, # (= nval_total) All valence bands must be included, not working yet for nval < nval_total 
+    # Response to calculate, see Doc. in responses.py
     response=1,
     components=["xx","yy","zz"],
 
-#   Turn on "split": WFN and RPMS calculation split: each proc is assigned to a task
-    split_by_proc=True,
+    #  WFN and RPMS calculation split by k-points
+    split_by_proc=False,
 
-    # These are the default parameters for the MPI runner.
+    # Default parameters for the MPI runner.
     # Please adapt them to your needs.
     nproc = 16,
     nproc_per_node = '',
@@ -54,18 +64,16 @@ flow = OPTflow(
 
 # Execution
 # This is my local execution task:
-#flow.write()
+flow.write()
 #
+
+# In case you need to load modules (in clusters), 
+# please modify this
 #modules="\
 #module swap gcc intel \n\
 #module load openmpi mkl\n\
 #module load intel/2013_sp1.4.211 openmpi hdf5/1.8.13-intel-p\n"
 #
-#mkjob_by_task(jobname='GaAs',
-#    cluster='catamount',
-#    nproc = flow.nproc,
-#    modules = modules
-#)
-
+# Not yet working:
 #flow.run()
 #flow.report()
